459542be9f6ce2a1246462612b79d27cde3fcfe1
Oxana <merenlin@gmail.com>
2013-08-20 22:20:53 +0200

    Code refactoring-2

--- todos/controllers/main.py
+++ todos/controllers/main.py
@@ -7,19 +7,19 @@ from flask import render_template, request, jsonify
 
 @app.route('/', methods=['GET'])
 def main():
-    """Index page loader."""
+    """ Index page loader. """
     return render_template('todos/index.html')
 
 @app.route('/api/', methods=['GET'])
 def get_todos():
-    """Fetching the current state of the db."""
+    """Fetching the current state of the db. """
     todos = Todo()
     items = todos.getAll()     
     return jsonify(todos=items)
 
 @app.route('/api/', methods=['POST'])
 def add_todo():
-    """Adding a new todo item with default user set"""
+    """ Adding a new todo item with default user set. """
     data = request.json
     todo = Todo(title=data['title'])
     todo_json = todo.add()
@@ -28,7 +28,7 @@ def add_todo():
 
 @app.route('/api/<todo_id>', methods=['GET'])
 def get_todo(todo_id):
-    """Get a todo by id call."""
+    """ Get a todo by id call. """
     todos = Todo()
     todo = todos.get(todo_id)
 
@@ -39,7 +39,7 @@ def get_todo(todo_id):
 
 @app.route('/api/<todo_id>', methods=['PUT'])
 def update_todo(todo_id):
-    """Update a todo by id call. New fields are in the request."""
+    """ Update a todo by id call. New fields are in the request. """
     data = request.json
     todos = Todo()
     todos.update(todo_id, data)
@@ -47,7 +47,7 @@ def update_todo(todo_id):
 
 @app.route('/api/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
-    """Delete a todo by id call."""
+    """ Delete a todo by id call. """
     todos = Todo()
     todos.delete(todo_id)
     return jsonify(response='OK')
